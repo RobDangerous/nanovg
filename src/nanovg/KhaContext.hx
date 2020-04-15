e36e24fca388e6bb6faf778965bbcb7ca0f5c940
Robert Konrad <git2016@robdangero.us>
2020-04-15 19:50:40 +0200

    Create textures

--- src/nanovg/KhaContext.hx
+++ src/nanovg/KhaContext.hx
@@ -1,8 +1,13 @@
 package nanovg;
 
+import kha.Image;
+
 class KhaContext {
 	public var flags: Int;
-	
+	public var textures = new Array<Image>();
+	public var view0: Float;
+	public var view1: Float;
+
 	public function new() {
 		
 	}
--- src/nanovg/KhaParams.hx
+++ src/nanovg/KhaParams.hx
@@ -1,6 +1,7 @@
 package nanovg;
 
 import haxe.ds.Vector;
+import kha.Image;
 
 class KhaParams extends NVGparams {
 	override public function renderCreate(uptr: Dynamic): Int {
@@ -8,11 +9,75 @@ class KhaParams extends NVGparams {
 		var align: Int = 4;
 		return 1;
 	}
-	override public function renderCreateTexture(uptr: Dynamic, type: Int, w: Int, h: Int, imageFlags: Int, data: Array<Int>): Int { return 0; }
-	override public function renderDeleteTexture(uptr: Dynamic, image: Int): Int { return 0; }
-	override public function renderUpdateTexture(uptr: Dynamic, image: Int, x: Int, y: Int, w: Int, h: Int, data: Array<Int>): Int { return 0; }
-	override public function renderGetTextureSize(uptr: Dynamic, image: Int, w: Ref<Int>, h: Ref<Int>): Int { return 0; }
-	override public function renderViewport(uptr: Dynamic, width: Float, height: Float, devicePixelRatio: Float): Void {}
+
+	override public function renderCreateTexture(uptr: Dynamic, type: Int, w: Int, h: Int, imageFlags: Int, data: Array<Int>): Int {
+		var context: KhaContext = uptr;
+		var tex = Image.create(w, h);
+
+		if (tex == null) return 0;
+
+		var pixels = tex.lock();
+		for (x in 0...w) {
+			for (y in 0...h) {
+				pixels.set(y * h * w * 4 + x * 4, data[y * h * w + x]);
+			}
+		}
+		tex.unlock();
+
+		context.textures.push(tex);
+		return context.textures.length - 1;
+	}
+
+	override public function renderDeleteTexture(uptr: Dynamic, image: Int): Int {
+		var context: KhaContext = uptr;
+
+		var tex = context.textures[image];
+		if (tex != null) {
+			tex.unload();
+			context.textures[image] = null;
+			return 1;
+		}
+
+		return 0;
+	}
+
+	override public function renderUpdateTexture(uptr: Dynamic, image: Int, x: Int, y: Int, w: Int, h: Int, data: Array<Int>): Int {
+		var context: KhaContext = uptr;
+
+		var tex = context.textures[image];
+		if (context.textures[image] != null) {
+			var pixels = tex.lock();
+			for (x in 0...w) {
+				for (y in 0...h) {
+					pixels.set(y * h * w * 4 + x * 4, data[y * h * w + x]);
+				}
+			}
+			tex.unlock();
+			return 1;
+		}
+
+		return 0;
+	}
+
+	override public function renderGetTextureSize(uptr: Dynamic, image: Int, w: Ref<Int>, h: Ref<Int>): Int {
+		var context: KhaContext = uptr;
+
+		var tex = context.textures[image];
+		if (context.textures[image] != null) {
+			w.value = tex.width;
+			h.value = tex.height;
+			return 1;
+		}
+
+		return 0;
+	}
+	
+	override public function renderViewport(uptr: Dynamic, width: Float, height: Float, devicePixelRatio: Float): Void {
+		var context: KhaContext = uptr;
+		context.view0 = width;
+		context.view1 = width;
+	}
+
 	override public function renderCancel(uptr: Dynamic): Void {}
 	override public function renderFlush(uptr: Dynamic): Void {}
 	override public function renderFill(uptr: Dynamic, paint: NVGpaint, compositeOperation: NVGcompositeOperationState, scissor: NVGscissor, fringe: Float, bounds: Vector<Float>, paths: Vector<NVGpath>, npaths: Int): Void {}
